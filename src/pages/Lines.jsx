7eed24d7e3bf206e75a8fba65a3b32d5589392d5
teran210c <162738757+teran210c@users.noreply.github.com>
2026-07-06 18:59:29 +0000

    Feat: set interactive opacity when cursor is over div

--- src/pages/Lines.jsx
+++ src/pages/Lines.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
+
 export default function Lines() {
+    const [activeLine, setActiveLine] = useState(null)
+
     const lines = ["SMT-L1", "SMT-L2", "SMT-L3", "SMT-L5"]
+
+    console.log(activeLine)
     return (
-        <div className="flex min-h-screen items-center">
+        <div className="flex flex-1 min-h-screen items-center">
             <ul>
                 {lines.map((line, index) => (
                     <li
                         key={index}
-                        className="m-4 p-3 bg-gray-300 w-50 text"
+                        className="m-4 p-3 bg-gray-300 w-50 cursor-pointer"
+                        onMouseEnter={() => setActiveLine(line)}
+                        onMouseLeave={() => setActiveLine(null)}
                     >
                         {line}
                     </li>
@@ -14,17 +22,26 @@ export default function Lines() {
                 ))}
             </ul>
             {/* futuro componente */}
-            <div className="flex flex-col items-center justify-center bg-gray-200 h-100 w-100">
-                <div id="L1-square" className="bg-gray-50 h-1/6 w-9/10 m-2">
+            <div className="flex flex-col items-center justify-center bg-gray-200 h-100 w-200 m-4">
+                <div 
+                    className={`bg-gray-500 h-1/6 w-9/10 m-2 ${activeLine === "SMT-L1" ? "opacity-100" : "opacity-40"}`}
+                >
 
                 </div>
-                <div id="L2-square" className="bg-gray-50 h-1/6 w-9/10 m-2">
+                <div
+                    className={`bg-gray-500 h-1/6 w-9/10 m-2 ${activeLine === "SMT-L2" ? "opacity-100" : "opacity-40"}`}
+                >
 
                 </div>
-                <div id="L3-square" className="bg-gray-50 h-1/6 w-9/10 m-2">
+                <div 
+                    className={`bg-gray-500 h-1/6 w-9/10 m-2 ${activeLine === "SMT-L3" ? "opacity-100" : "opacity-40"}`}
+                >
 
                 </div>
-                <div id="L5-square" className="bg-gray-50 h-1/6 w-9/10 m-2">
+                <div 
+                    className={`bg-gray-500 h-1/6 w-9/10 m-2 ${activeLine === "SMT-L5" ? "opacity-100" : "opacity-40"}`}
+
+                >
 
                 </div>
 
